ca0e7fc366e95c55646517c59eaba5fc987e972c
lsrujan <srujanlanderi@gmail.com>
2024-11-30 19:20:39 +0530

    Auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,61 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./pages/Home"
-import React, { useEffect } from 'react';
+import Navigation from "./components/Navigation"
+import React, {useEffect, useState} from 'react';
 import { Loader2 } from 'lucide-react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
+  
+  useEffect(() => {
+
+    if(isLoggedIn) {
+      setIsLoading(false);
+      return
+    }
+
+    const checkLogin = async () => {
+      const tokenData = JSON.parse(localStorage.getItem('tokenInfo'));
+      if(!tokenData) {
+        setIsLoading(false);
+        console.log("No token data found");
+        localStorage.removeItem('tokenInfo');
+        localStorage.removeItem('userInfo');
+        return;
+      }
+
+      const userResponse = await fetch('http://localhost:8000/api/auth/user-data', {
+        headers: {
+          'Authorization': `Bearer ${tokenData.access_token}`,
+          'Accept': 'application/json'
+        }
+      });
+      console.log(userResponse)
+      if (!userResponse.ok) {
+        setIsLoading(false);
+        console.log("No User data found");
+        localStorage.removeItem('tokenInfo');
+        localStorage.removeItem('userInfo');
+        return;
+      }
+
+      const userData = await userResponse.json();
+      console.log(userData)
+      console.log("Login Status: ", true);
+      setIsLoggedIn(true);
+      setIsLoading(false);
+    }
+
+    checkLogin();
+  }, []);
+
   return (
     <Router>
-
       <ToastContainer
         position="bottom-left"
         autoClose={3000}
@@ -22,12 +68,17 @@ function App() {
         pauseOnHover
         theme="light"
       />
-
       <div className="min-h-screen bg-gray-100">
+        <Navigation isLoggedIn = {isLoggedIn} setIsLoggedIn = {setIsLoggedIn} setIsLoading={setIsLoading}/>
         {
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
+          isLoading ? (
+            <div className="min-h-screen flex items-center justify-center">
+              <Loader2 className="h-12 w-12 animate-spin text-indigo-600" />
+            </div>
+          ):
+          <Routes>
+            <Route path="/" element={<Home/>} />
+          </Routes>
         }
       </div>
     </Router>
